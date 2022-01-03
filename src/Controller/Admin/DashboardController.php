1a5c60cc59120e7de1e3592a6988ec6d71d5663c
Daixois <tom@gouxdimas.com>
2022-01-03 14:35:18 +0100

    css register

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Perso;
 use App\Entity\Theme;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -32,5 +33,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Theme', 'fas fa-list', Theme::class);
         yield MenuItem::linkToCrud('Personnages', 'fas fa-list', Perso::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
     }
 }
